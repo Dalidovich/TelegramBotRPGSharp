6a30bf39175061c5c930a9de1b7049063e898cba
pops375 <pops375@yandex.ru>
2022-05-20 21:52:32 +0300

    add places for fight fight completed without hp potion

--- TelegramBotRPG/InlineButtons.cs
+++ TelegramBotRPG/InlineButtons.cs
@@ -29,6 +29,40 @@ namespace TelegramBotRPG
                 }
             );
         }
+        public static IReplyMarkup GetButtonsOnFight()
+        {
+            return new InlineKeyboardMarkup
+            (
+                new List<List<InlineKeyboardButton>>
+                {
+                    new List<InlineKeyboardButton>
+                    {
+                        new InlineKeyboardButton("1") { Text = "atack",CallbackData="a"}, new InlineKeyboardButton("2") { Text = "dodge",CallbackData = "d" }
+                    },
+                    new List<InlineKeyboardButton>
+                    {
+                        new InlineKeyboardButton("3") { Text = "use hp potion",CallbackData = "uhpf" } //call back data means fight
+                    }
+                }
+            );
+        }
+        public static IReplyMarkup GetButtonsOnDodge()
+        {
+            return new InlineKeyboardMarkup
+            (
+                new List<List<InlineKeyboardButton>>
+                {
+                    new List<InlineKeyboardButton>
+                    {
+                        new InlineKeyboardButton("1") { Text = "atack",CallbackData="a"}, new InlineKeyboardButton("2") { Text = "escape",CallbackData = "de" }
+                    },
+                    new List<InlineKeyboardButton>
+                    {
+                        new InlineKeyboardButton("3") { Text = "use hp potion",CallbackData = "uhpd" } //call back data means dodge
+                    }
+                }
+            );
+        }
         public static IReplyMarkup GetButtonsOnEndGame()
         {
             return new InlineKeyboardMarkup
